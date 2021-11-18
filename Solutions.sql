8c2894bfc60b5a45d335392603eddf7033c22e0f
Manal <manalalzeer01@gmail.com>
2021-11-18 16:36:58 +0300

    Update Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -4,33 +4,33 @@ where mov_id =  905 or mov_id = 907 or mov_id= 917;
 
 --Q2
 select actor.act_fname,actor.act_lname,movie_cast.role
-fromactor
-join movie_cast ON actor.act_id=movie_cast.act_id
-join movie ON movie_cast.mov_id=movie.mov_id AND movie.mov_title='Annie Hall';
+from actor
+join movie_cast on actor.act_id=movie_cast.act_id
+join movie on movie_cast.mov_id=movie.mov_id and movie.mov_title='Annie Hall';
 
 --Q3
 select act_fname , act_lname ,mov_title , mov_year
 from movies.movie
-INNER JOIN movies.actor
-on not movies.movie.mov_year BETWEEN 1999 AND 2000;
+INNER join movies.actor
+on not movies.movie.mov_year between 1999 and 2000;
 
 --Q4
 select mov_year
 from movies.movie
-INNER JOIN movies.rating
+INNER join movies.rating
 on movies.rating.rev_stars = 3 or movies.rating.rev_stars = 4
 ORDER BY mov_year ASC;
 
 --Q5
 select mov_id, mov_title , mov_year
 from movies.movie
-where mov_title LIKE '%Boogie Nights%'
+where mov_title like '%Boogie Nights%'
 order by mov_year ASC;
 
 --Q6
 select mov_title , dir_fname , dir_lname ,rating.num_o_ratings
 from movies.director
-JOIN movies.movie_direction
+join movies.movie_direction
 on movies.director.dir_id = movies.movie_direction.dir_id
 join movies.movie
 on movies.movie.mov_id = movie_direction.mov_id
